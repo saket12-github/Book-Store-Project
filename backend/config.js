ede0fc7f65372d6c6786feed7c03c5c26e954319
Saket Chaudhari <saketchaudhari12@gmail.com>
2025-04-16 21:53:11 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://spchaudhari149:Mongodb123@bookstore-mern.5ttq37u.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
